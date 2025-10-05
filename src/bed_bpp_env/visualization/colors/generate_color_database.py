56f53851b5f7fa5a83224613d3e17703bc26c4e5
Florian Enzenhofer <>
2025-10-05 12:20:33 +0200

    Autoformat `generate_color_database`

--- src/bed_bpp_env/visualization/colors/generate_color_database.py
+++ src/bed_bpp_env/visualization/colors/generate_color_database.py
@@ -1,6 +1,7 @@
-'''
+"""
 This script generates for a given benchmark data file the corresponding database that stores the colors for the items within an order.
-'''
+"""
+
 import utils
 import json
 import logging
@@ -10,14 +11,14 @@ import tqdm
 logger = logging.getLogger(__name__)
 
 
-def generateColorDatabase(indataorders:pathlib.Path) -> None:
-    '''
+def generateColorDatabase(indataorders: pathlib.Path) -> None:
+    """
     Generates a json file for the given orders
 
     Parameters.
     -----------
-    indataorders: pathlib.Path  
-      The orders that are currently invesigated.  
+    indataorders: pathlib.Path
+      The orders that are currently invesigated.
 
     Output.
     -------
@@ -32,7 +33,7 @@ def generateColorDatabase(indataorders:pathlib.Path) -> None:
             ...
         }
     }
-    '''
+    """
     colorsAvailable = utils.getPathToExampleData().parent.joinpath("code/visualization/colors/colors.json")
 
     # load input data orders and the color base
@@ -42,15 +43,15 @@ def generateColorDatabase(indataorders:pathlib.Path) -> None:
         COLORBASE = json.load(file, parse_int=False)
 
     # tbd
-    colorDatabase = {} # keys: order id, values: dict with article as key and color name as value
+    colorDatabase = {}  # keys: order id, values: dict with article as key and color name as value
     logger.info(f"create the color database for {indataorders}.")
     for orderId, orderData in tqdm.tqdm(ORDERS_IN.items()):
         colorsInOrder = {}
-        
+
         for item in orderData["item_sequence"].values():
             article = item["article"]
-            if not(article in colorsInOrder.keys()):
-                keyInColorbase = f"own_{str((len(colorsInOrder)%len(COLORBASE))+1).zfill(2)}"
+            if not (article in colorsInOrder.keys()):
+                keyInColorbase = f"own_{str((len(colorsInOrder) % len(COLORBASE)) + 1).zfill(2)}"
                 colorsInOrder[article] = keyInColorbase
 
         colorDatabase[orderId] = colorsInOrder
@@ -60,4 +61,3 @@ def generateColorDatabase(indataorders:pathlib.Path) -> None:
     logger.info(f"store the color database as {colordbFile}")
     with open(colordbFile, "w") as file:
         json.dump(colorDatabase, file)
-

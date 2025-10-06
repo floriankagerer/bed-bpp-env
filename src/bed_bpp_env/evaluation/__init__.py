d198327ff1f7647169e735bd12a3d574f7954b1b
Florian Enzenhofer <>
2025-10-06 20:19:08 +0200

    Fix imports

--- src/bed_bpp_env/evaluation/__init__.py
+++ src/bed_bpp_env/evaluation/__init__.py
@@ -15,8 +15,3 @@ EVALOUTPUTDIR.mkdir(exist_ok=True)
 FILE_KPI_DEFINITION = pathlib.Path(__file__).parent.resolve().joinpath("kpi_definition.yaml")
 with open(FILE_KPI_DEFINITION) as file:
     KPI_DEFINITION = yaml.load(file, Loader=yaml.loader.SafeLoader)
-
-
-from evaluation.KPIs import KPIs
-from evaluation.PackingPlanEvaluator import PackingPlanEvaluator
-from evaluation.blender import *

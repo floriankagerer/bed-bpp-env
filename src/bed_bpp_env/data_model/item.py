2dca3ed8734834c6683d4b2baecb5451c9352e0c
Florian Enzenhofer <>
2025-10-07 14:01:31 +0200

    Add data model for an item within the benchmarking dataset

---
+++ src/bed_bpp_env/data_model/item.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field, fields
+
+
+@dataclass
+class Item(object):
+    """Represents an object that has to be packed and that is part of an order."""
+
+    article: str
+    """The article name of this item."""
+    id: str
+    """The identifier of this item."""
+    product_group: str
+    """The product group this item belongs to."""
+    length_mm: int = field(metadata={"alias": "length/mm"})
+    """The length of this item in millimeters."""
+    width_mm: int = field(metadata={"alias": "width/mm"})
+    """The width of this item in millimeters."""
+    height_mm: int = field(metadata={"alias": "height/mm"})
+    """The height of this item in millimeters."""
+    weight_kg: float = field(metadata={"alias": "weight/kg"})
+    """The weight of this item in kilograms."""
+    sequence: int
+    """The position of this item within the item sequence."""
+
+    def to_dict(self) -> dict[str, str | int | float]:
+        """Converts the object to a dictionary."""
+        item_to_dict = {}
+
+        for item_field in fields(self):
+            key = item_field.metadata.get("alias", item_field.name)
+            item_to_dict[key] = getattr(self, item_field.name)
+
+        return item_to_dict
+
+    @classmethod
+    def from_dict(cls, serialized: dict[str, str | int | float]) -> Item:
+        """Deserialize dictionary into dataclass instance."""
+        init_kwargs = {}
+        for f in fields(cls):
+            alias = f.metadata.get("alias", f.name)
+            # Support either the alias or the original name in the input dict
+            if alias in serialized:
+                init_kwargs[f.name] = serialized[alias]
+            elif f.name in serialized:
+                init_kwargs[f.name] = serialized[f.name]
+        return cls(**init_kwargs)

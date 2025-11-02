cf4636ef10c9d0fef87d267b42d0c6abaae9d0a8
Florian Enzenhofer <>
2025-11-02 21:06:32 +0100

    Add function to deserialize packing plans

--- src/bed_bpp_env/io_utils.py
+++ src/bed_bpp_env/io_utils.py
@@ -4,6 +4,7 @@ import json
 from pathlib import Path
 
 from bed_bpp_env.data_model.order import Order
+from bed_bpp_env.data_model.packing_plan import PackingPlan
 
 
 def load_order_sequence(file_path: Path) -> list[Order]:
@@ -27,3 +28,25 @@ def load_order_sequence(file_path: Path) -> list[Order]:
         order_sequence.append(order_i)
 
     return order_sequence
+
+
+def load_packing_plan_sequence(file_path: Path) -> list[PackingPlan]:
+    """Loads a packing plan sequence that is stored in the given file path.
+
+    Args:
+        file_path (Path): The path to the file that contains the packing plans.
+
+    Returns:
+        list[PackingPlan]: The deserialized packing plan sequence.
+    """
+    with open(file_path) as file:
+        serialized_packing_plan_sequence: dict = json.load(file, parse_int=False)
+
+    packing_plan_sequence = []
+
+    for packing_plan_id, actions in serialized_packing_plan_sequence.items():
+        serialized = {"id": packing_plan_id, "actions": actions}
+        packing_plan_i = PackingPlan.from_dict(serialized)
+        packing_plan_sequence.append(packing_plan_i)
+
+    return packing_plan_sequence

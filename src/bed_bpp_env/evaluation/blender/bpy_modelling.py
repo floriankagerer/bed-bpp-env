465988483d22d691d10652dd33f14d6bd8113f37
Florian Enzenhofer <>
2025-11-06 20:49:26 +0100

    Add function that add a box to Blender

--- src/bed_bpp_env/evaluation/blender/bpy_modelling.py
+++ src/bed_bpp_env/evaluation/blender/bpy_modelling.py
@@ -4,7 +4,18 @@ import math
 
 import bpy  # type: ignore
 
-from bed_bpp_env.evaluation.blender.bpy_object_properties import set_material_and_enable_rigid_body
+from bed_bpp_env.data_model.action import Action
+from bed_bpp_env.data_model.length_unit import LengthUnit
+from bed_bpp_env.data_model.orientation import Orientation
+from bed_bpp_env.data_model.type_alias import RGBAColor
+from bed_bpp_env.evaluation.blender.bpy_object_properties import (
+    assign_material_to_object,
+    define_and_get_material,
+    get_material_from_bpy_data,
+    set_material_and_enable_rigid_body,
+    set_rigid_body_settings,
+)
+from bed_bpp_env.evaluation.blender.collision_shape import CollisionShape
 
 # TODO(florian): Add enum for targets
 
@@ -131,3 +142,51 @@ def add_rollcontainer_to_blender() -> None:
     wheel_positions = [(0.1, 0.1), (0.1, 0.6), (0.7, 0.6), (0.7, 0.1)]
     for pos in wheel_positions:
         _add_rollcontainer_wheel(pos)
+
+
+def add_box_to_blender(action: Action, color_rgba: RGBAColor) -> None:
+    """
+    Adds the item to the given position. The item gets the given color assigned.
+
+    Args:
+        action (Action): Contains which item is placed, in which location, and in which orientation.
+        color_rgba (RGBAColor): The rgba values of the item's color in Blender.
+    """
+    item = action.item
+
+    orientation = Orientation(action.orientation)
+
+    size_wrt_orientation = orientation.get_item_size(item)
+    size_wrt_orientation_in_m = [LengthUnit.MILLIMETER.convert(dim, LengthUnit.METER) for dim in size_wrt_orientation]
+
+    scaled_flb_coordinates = [
+        LengthUnit.MILLIMETER.convert(coord, LengthUnit.METER) for coord in action.flb_coordinates.xyz
+    ]
+
+    location = [coord + dim / 2 for coord, dim in zip(scaled_flb_coordinates, size_wrt_orientation_in_m)]
+
+    # add the item
+    bpy.ops.mesh.primitive_cube_add(
+        size=1.0,
+        calc_uvs=True,
+        enter_editmode=False,
+        align="WORLD",
+        location=location,
+        rotation=(0.0, 0.0, 0.0),
+        scale=size_wrt_orientation_in_m,
+    )
+
+    set_rigid_body_settings(enable=True, mass=item.weight_kg, collision_shape=CollisionShape.BOX)
+
+    # Use id for object name
+    bpy.context.object.name = f"item_{item.id}"
+
+    # set the color and material, respectively
+    ob = bpy.context.active_object
+
+    # Get material
+    material = get_material_from_bpy_data(item.id)
+    if material is None:
+        material = define_and_get_material(item.id, color_rgba)
+
+    assign_material_to_object(ob, material)

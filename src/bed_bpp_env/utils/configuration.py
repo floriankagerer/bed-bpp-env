390ba60c3609d1e9dbad9579224ffb64d11f6b5a
Florian Enzenhofer <>
2025-11-10 21:49:25 +0100

    Refactor module `configuration`

--- src/bed_bpp_env/utils/configuration.py
+++ src/bed_bpp_env/utils/configuration.py
@@ -9,29 +9,30 @@ Access the variables via the module `utils`.
 import configparser
 import datetime
 import logging
-import pathlib
 import shutil
+from pathlib import Path
 
 __logger = logging.getLogger(__name__)
 
 # define the names of the different files/folders
-fnameConfiguration = "bed-bpp_env.conf"
+FILENAME_CONFIGURATION = "bed-bpp_env.conf"
+"""The filename of the configuration file."""
 currentOutputfname = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 fnameLogging = "logs.log"
 
 # create the path to the configuration file
-dirFile = pathlib.Path(__file__).parent.resolve()
+dirFile = Path(__file__).parent.resolve()
 dirConfigFile = dirFile.parent.resolve()  # go one level/folder up
-configFile = pathlib.Path.joinpath(dirConfigFile, fnameConfiguration)
+configFile = Path.joinpath(dirConfigFile, FILENAME_CONFIGURATION)
 
 # create the outputfolder
 dirOutput = dirFile.parents[2].resolve()  # go three levels up
-dirOutput = pathlib.Path.joinpath(dirOutput, f"output/{datetime.datetime.now().strftime('%Y-%m-%d')}")
-OUTPUTDIR = pathlib.Path.joinpath(dirOutput, currentOutputfname)
+dirOutput = Path.joinpath(dirOutput, f"output/{datetime.datetime.now().strftime('%Y-%m-%d')}")
+OUTPUTDIR = Path.joinpath(dirOutput, currentOutputfname)
 OUTPUTDIR.mkdir(parents=True, exist_ok=True)
 
 # configure the logging module
-LOGGING_FILE = pathlib.Path.joinpath(OUTPUTDIR, fnameLogging)
+LOGGING_FILE = Path.joinpath(OUTPUTDIR, fnameLogging)
 
 # read the configuration from the previously defined directory
 ENTIRECONFIG = configparser.ConfigParser()
@@ -42,7 +43,25 @@ ENTIRECONFIG.read(configFile)
 # copy the used configuration file
 shutil.copy(configFile, OUTPUTDIR)
 __logger.info(f"copied the configuration to {OUTPUTDIR}")
-USEDCONFIGURATIONFILE = pathlib.Path.joinpath(OUTPUTDIR, fnameConfiguration)
+USEDCONFIGURATIONFILE = Path.joinpath(OUTPUTDIR, FILENAME_CONFIGURATION)
 
-if __name__ == "__main__":
-    pass
+
+def copy_configuration_file_to_output_directory(output_dir: Path) -> Path:
+    """
+    Copies the configuration file to the output directory.
+
+    Args:
+        output_directory (Path): The output directory.
+
+    Returns:
+        Path: The path to the copied configuration file.
+    """
+    config_file_path = Path(__file__).parents[1] / FILENAME_CONFIGURATION
+
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+    copied_config_file_path = output_dir / FILENAME_CONFIGURATION
+    shutil.copy(config_file_path, output_dir)
+
+    return copied_config_file_path
